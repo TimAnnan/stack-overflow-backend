b70e81bf6465bd9de3b8447f5a4a240c511f3c1a
TimAnnan <anandvariar59@gmail.com>
2023-12-01 16:07:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# `npm i`
+use the above command in the client side to install the dependencies
+
+# ` npm i`
+use the above command on the server side to install dependencies
+
+# `npm start`
+use the above command on the client side to run client
+
+# `npm start`
+use the above command tp run the server side
---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import questionRoutes from './routes/Questions.js'
+import userRoutes from './routes/users.js'
+import answerRoutes from './routes/Answers.js'
+import dotenv from 'dotenv'
+const app = express(); //creating server
+dotenv.config()
+app.use(express.json({limit: "30mb", extended: true}))
+app.use(express.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
+
+app.get('/',(req, res) => {
+    res.send("This is a clone API")
+})
+
+app.use('/user', userRoutes)
+app.use('/questions', questionRoutes)
+app.use('/answer', answerRoutes)
+const PORT = process.env.PORT || 5000
+
+const DATABASE_URL = process.env.CONNECTION_URL
+
+mongoose.connect( DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => { console.log(`server running on port ${PORT}`)}))
+.catch((err)  => console.log(err.message))
